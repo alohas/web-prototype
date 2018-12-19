27c904ffce225c716319e26f425afaf642cb4d8d
Dominykas Kutka <dominykaskulka@yahoo.com>
2018-12-19 13:29:34 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.display = " ";
+    document.getElementById("mySidenav").style.width = "350px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.display = " ";
+}
